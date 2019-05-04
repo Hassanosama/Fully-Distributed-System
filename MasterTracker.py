ea118e5f1983759e700735cb3eca78ea3729dbdd
Hassanosama <hassanosama007@gmail.com>
2019-05-04 23:30:04 +0200

    Merged
    Merged database part with distributed system part, in client file.
    changed the file system in the master tracker to a data base system.

--- MasterTracker.py
+++ MasterTracker.py
@@ -3,13 +3,14 @@ import time
 import sys
 import threading
 import queue
+import mysql.connector
 
 print('Establishing Master Tracker,Please Wait..')
 
 #Initializing Variables.
 RecordsFile = open('Records.txt','r')   #Load records.
 LookUpTable = {}
-VideoNames = {""}
+VideoNames = []
 Instance = []
 MinumumNumberOfCopies = 3
 q = queue.Queue()
@@ -36,6 +37,14 @@ for i in range(0,NumberOfThreads):
     subscribers[i].connect('tcp://%s:%s' %(IP[i],Ports[i]) )
     subscribers[i].setsockopt(zmq.SUBSCRIBE, b'')
     msg.append('')
+
+#Connecting to the database.
+try:
+    mydb = mysql.connector.connect(host="localhost",user="root",passwd="1234",database="os_project")
+    mycursor = mydb.cursor()
+except:
+    print("Can't connect to database")
+
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 def Connect(ID):
     msg[ID] = subscribers[ID].recv_string()
@@ -53,20 +62,27 @@ def Connect(ID):
                 FileName = subscribers[ID].recv_string()
                 FilePath = subscribers[ID].recv_string()
                 PortNumber = subscribers[ID].recv_string()
+                '''
                 LookUpTable.update({FileName :[ID, FilePath,State[ID]]})
                 RecordsFile.write(FileName+'|'+str(ID))
                 Instance.append(FileName+'|'+str(ID))
                 VideoNames.add(FileName)
+                '''
+                #Insert that file record to the records table.
+                mycursor.execute("INSERT INTO records (filename,datanode_id,filepath) VALUES(%s,%d,%s);",(FileName,ID,FilePath))
+
+                #Update the lookup table.
+                mycursor.execute("INSERT INTO look_up (user_id,filename,datanode_id,filepath,dataNode_state) VALUES(%d,%s,%d,%s,%s);",(1,FileName,ID,FilePath,State[ID]))            
+                
+                VideoNames.append(FileName)
                 q.put(FileName)
 
             else:
                 PortNumber = subscribers[ID].recv_string()
 
             TransferPorts[PortNumber] = 'free'
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#----------------------------------------------------------------------------------------------
 
-
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 def GetFreePort(idx):
     if (State[idx] == 'offline'):
         return 'none','none'
@@ -75,7 +91,7 @@ def GetFreePort(idx):
         if(TransferPorts[p] == 'free'):
             return p,IP[idx]
     return 'none','none'
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#----------------------------------------------------------------------------------------------
 def ClientsHandler():
     while True:
         request = Server.recv_string() #Waiting for request from any client.
@@ -109,13 +125,51 @@ def ClientsHandler():
                         TransferPorts[FreePort] = 'busy'
             print('Ports Found')
             Server.send_string(Info)
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-def GetSourceNode(VideoName):
+#----------------------------------------------------------------------------------------------
+def GetSourceNode(name):
+    mycursor.execute("SELECT datanode_id and filepath FROM reocrds WHERE filename = %s",name)
+    sources = mycursor.fetchall()
+    #We can get more than one source, so we should iterate to find an available one.
+    while True:
+        for src in sources:
+            DataNodeIdx = src[0]
+            if State[DataNodeIdx] == 'online' and DataNodeAsSource[DataNodeIdx] == 'no': #The machine should be online and not replicating for now.
+                return src  #return the index of the datanode and the path of the file on that datanode
+
+    '''
+
     for i in range(0,NumberOfThreads):
         if State[i] == 'online' and DataNodeAsSource[i] == 'no' and Instance.__contains__(VideoName +'|'+ str(i)):
             DataNodeAsSource[i] == 'yes'
             return i
-def GetChosenNodes(VideoName):
+    '''
+def GetChosenNodes(name):
+    Info = ''
+    mycursor.execute("SELECT datanode_id FROM reocrds WHERE filename = %s",name)
+    sources = mycursor.fetchall()
+    counter = len(sources)  #Number of copies till now.
+
+    #Iterating to find the data nodes which are NOT in the sources array.
+    for DataNodeIdx in range (0,NumberOfThreads):
+
+        if(counter == MinumumNumberOfCopies):   #Check if the number of copies of the wanted fill reached the minimum wanted number or not.
+            break
+        if State[DataNodeIdx] == 'online' and not sources.__contains__(DataNodeIdx):
+            FreePort = 'none'
+            while FreePort == 'none':   #Stalling till get a free port from that data node.
+                FreePort,PortIP = GetFreePort(DataNodeIdx)
+            TransferPorts[FreePort] = 'busy'
+            cnounter+=1     #Increase the number of copies.
+            Info += ' '
+            Info += PortIP
+            Info += ' '
+            Info += FreePort
+
+    return Info
+
+
+
+    '''
     Info = ''
     counter = 1
     for i in range(0,NumberOfThreads):
@@ -135,8 +189,9 @@ def GetChosenNodes(VideoName):
             Info += FreePort
             
     return Info
+    '''
 
-def StartCopying(SourceNode,ChosenNodes,VideoName):
+def StartCopying(SourceNode,ChosenNodes,FilePath):
 
     ToDataNode = context.socket(zmq.REQ)
     ToDataNode.connect ("tcp://%s:%s" % (IP[SourceNode],DataNodeAsServerPort))
@@ -144,32 +199,46 @@ def StartCopying(SourceNode,ChosenNodes,VideoName):
     ToDataNode.send_string(ChosenNodes)
     ToDataNode.recv_string()
 
-    ToDataNode.send_string(VideoName)
+    ToDataNode.send_string(FilePath)        #Sending the file path to the data node to be able access it and send it to the other chosen data nodes.
     ToDataNode.recv_string()        #Note: the source node will not send a respond till it finished the copying porcess.
 
 def CountCopies(name):
+    mycursor.execute("SELECT COUNT(*) FROM reocrds WHERE filename = %s",name)
+    counter = mycursor.fetchone()
+    return counter
+    '''
     counter = 0
     for i in range(0,NumberOfThreads):
         if(Instance.__contains__(name+'|'+str(i))):
             counter+=1
+    
     return counter
+    '''
 
 
 def ManageRuplicating():
     
-    for x in VideoNames:
-        if(CountCopies(x) < MinumumNumberOfCopies):
-            q.put(x)
+    for name in VideoNames:
+        if(CountCopies(name) < MinumumNumberOfCopies):
+            q.put(name)
 
     while True:
         while q.qsize() > 0:
             VideoName = q.get()
-            SourceNode = GetSourceNode(VideoName)
+            SourceNode = GetSourceNode(VideoName)       #SourceNode contains = [datanode_id , file path]
             ChosenNodes = GetChosenNodes(VideoName)
-            StartCopying(SourceNode,ChosenNodes,VideoName)
-            DataNodeAsSource[SourceNode] = 'no'
+            StartCopying(SourceNode[0],ChosenNodes,SourceNode[1])
+            DataNodeAsSource[SourceNode[0]] = 'no'
 
-#..........................................................................Main..........................................................................
+#------------------------------Main------------------------------#
+
+
+
+#Get the records from the database to iterates on the video's name.
+mycursor.execute("SELECT DISTINCT filename FROM records;")
+VideoNames = mycursor.fetchall()
+
+'''
 RecordsFile = open('Records.txt','a')   #Load records.
 for record in RecordsFile:
     record = str(record)
@@ -179,6 +248,7 @@ for record in RecordsFile:
         name,dummy = record.split('|')
         VideoNames.add(name)
 RecordsFile.close()
+'''
 
 
 for i in range(0,NumberOfThreads):  #Running a thread for every connected data node.
